0bedc1db4831b4b9654458f83e1c3db7b494dab9
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-01-08 00:05:14 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  distDir: "build",
+
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] },
+    });
+    return cfg;
+  },
+};
